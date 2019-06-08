7c9a230b09b1058e677670a23ca70efc1e9d3ed8
deeraj.bommaraju <deerajchakravarthy@gmail.com>
2019-06-08 11:29:33 +0530

    Add project files.

---
+++ TestApp/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestApp.Models
+{
+    public partial class Student
+    {
+        public long StudentId { get; set; }
+        public string StudentName { get; set; }
+    }
+}
---
+++ TestApp/Models/StudentContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TestApp.Models
+{
+    public partial class StudentContext : DbContext
+    {
+        public StudentContext()
+        {
+        }
+
+        public StudentContext(DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Student { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=192.168.1.102\\SQLEXPRESS01;Database=Student;Trusted_Connection=False; User Id = Test;Password=Test*1234;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.StudentId).HasColumnName("StudentID");
+
+                entity.Property(e => e.StudentName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
